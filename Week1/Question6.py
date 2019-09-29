6207faf57b21e82e7c6f5e4899102bb17fad711e
johnsn27 <nathan.johnson@bbc.co.uk>
2019-09-29 16:57:58 +0100

    Question 6

--- Week1/Question6.py
+++ Week1/Question6.py
@@ -1,17 +1,25 @@
-def question3():
-    x = input("Give an x value: ")
-    y = input("Give a y value: ")
-    z = input("Give an z value: ")
-    if x == y == z:
+def getSum(piece):
+    if len(piece) == 0:
+        return 0
+    else:
+        return piece[0] + getSum(piece[1:])
+
+
+def question6(f, x, y, z):
+    xSum = f(x)
+    ySum = f(y)
+    zSum = f(z)
+
+    if xSum == ySum == zSum:
         print("All three inputs have equal values")
-    elif x == y:
+    elif xSum == ySum:
         print("x and y have equal values")
-    elif x == z:
+    elif xSum == zSum:
         print("x and z have equal values")
-    elif y == y:
+    elif ySum == zSum:
         print("y and z have equal values")
     else:
         print("All the three inputs have different values")
 
 
-question3()
+question6(getSum, [1, 2, 3], [10, 20, 30, 40], [65, 32, 3])
