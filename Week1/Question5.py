94cb6f106a2fa13e727d8cf16139eaa456de44f1
johnsn27 <nathan.johnson@bbc.co.uk>
2019-09-29 16:57:41 +0100

    Question 5

--- Week1/Question5.py
+++ Week1/Question5.py
@@ -1,17 +1,25 @@
-def question3():
-    x = input("Give an x value: ")
-    y = input("Give a y value: ")
-    z = input("Give an z value: ")
-    if x == y == z:
+def getSum(array):
+    if len(array) == 0:
+        return 0
+    else:
+        return array[0] + getSum(array[1:])
+
+
+def question5(x, y, z):
+    xSum = getSum(x)
+    ySum = getSum(y)
+    zSum = getSum(z)
+
+    if xSum == ySum == zSum:
         print("All three inputs have equal values")
-    elif x == y:
+    elif xSum == ySum:
         print("x and y have equal values")
-    elif x == z:
+    elif xSum == zSum:
         print("x and z have equal values")
-    elif y == y:
+    elif ySum == zSum:
         print("y and z have equal values")
     else:
         print("All the three inputs have different values")
 
 
-question3()
+question5([1, 2, 3], [10, 20, 30, 40], [65, 32, 3])
